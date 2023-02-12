ad4721c88b4b9fc2db5208381f78883ed18a977c
Aid Idrizović <idrizovicaid@gmail.com>
2023-02-12 02:11:08 -0600

    finalize ui

--- src/components/VideoComponent.tsx
+++ src/components/VideoComponent.tsx
@@ -1,17 +1,59 @@
-import { Flex, FlexProps, Image } from '@chakra-ui/react'
+import React, { useRef, useEffect } from 'react';
+import { Flex, FlexProps } from '@chakra-ui/react'
 
-export const VideoComponent = (props: FlexProps) => (
-    <Flex justifyContent="center" width="100%" background="#77A59B">
-        <Image
-            style={{ transition: 'opacity 700ms linear', opacity: '0' }}
-            onLoad={e => (e.currentTarget.style.opacity = '1')}
-            loading="lazy"
-            height="100vh"
-            maxHeight="-webkit-fill-available"
-            shadow="md"
-            objectFit="cover"
-            src="https://picsum.photos/500/800"
-            alt="video"
-        />
-    </Flex>
-)
+export const VideoComponent = (props: FlexProps) => {
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    useEffect(() => {
+        let observer: IntersectionObserver | null = null;
+
+        if (videoRef.current) {
+            observer = new IntersectionObserver(entries => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        videoRef.current!.play();
+                    } else {
+                        videoRef.current!.pause();
+                    }
+                });
+            });
+
+            observer.observe(videoRef.current);
+
+            if (videoRef.current!.offsetParent !== null) {
+                videoRef.current!.play();
+            }
+        }
+
+        return () => {
+            if (observer) {
+                observer.disconnect();
+            }
+        };
+    }, []);
+
+
+    const handlePlayPause = () => {
+        if (videoRef.current) {
+            if (videoRef.current.paused) {
+                videoRef.current.play();
+            } else {
+                videoRef.current.pause();
+            }
+        }
+    };
+
+    return (
+        <Flex justifyContent="center" width="100%" height="100vh" background="#77A59B">
+            <video
+                loop
+                muted
+                onClick={handlePlayPause}
+                style={{ cursor: 'pointer' }}
+                ref={videoRef}
+                src="https://assets.mixkit.co/videos/preview/mixkit-waves-in-the-water-1164-large.mp4"
+                height="100%"
+            />
+        </Flex>
+    );
+}
