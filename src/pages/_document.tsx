ffb1eaac5476edb015b9dbcf5af9369ef9feb1a5
Aid Idrizović <idrizovicaid@gmail.com>
2023-02-13 15:57:55 -0600

    add title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,10 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head title="Generic Page">
+        <Head>
             <link rel="shortcut icon" href="https://iili.io/HEwxs7n.png" />
             <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+            <title>Generic Page</title>
         </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
