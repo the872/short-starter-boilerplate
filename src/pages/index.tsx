22a1cef2306384251829017b7b66325373202086
Aid Idrizović <idrizovicaid@gmail.com>
2023-02-13 15:54:42 -0600

    add analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { Scroller } from '../components/Scroller'
 import { Container } from '../components/Container'
+import { Analytics } from '@vercel/analytics/react';
 
 const Index = () => (
   <Container height="100vh" maxHeight="-webkit-fill-available">
-    <Scroller />
+      <Scroller />
+      <Analytics />
   </Container>
 )
 
