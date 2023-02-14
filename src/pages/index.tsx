2401bdd9a6ce52dabb87543093d661f018fefb3a
Aid Idrizović <idrizovicaid@gmail.com>
2023-02-13 22:05:45 -0600

    ss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Container } from '../components/Container'
 import { Analytics } from '@vercel/analytics/react';
 
 const Index = () => (
-  <Container height="100vh" maxHeight="-webkit-fill-available">
+  <Container height="100vh" maxHeight="-webkit-fill-available" backgroundColor="#9B76AA">
       <Scroller />
       <Analytics />
   </Container>
