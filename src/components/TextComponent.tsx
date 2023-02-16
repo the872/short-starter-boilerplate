febfeb5cb2bc6c0a98e7f71d5c1aa6b3817a8863
Aid Idrizović <idrizovicaid@gmail.com>
2023-02-15 21:48:00 -0600

    push error handling

--- src/components/TextComponent.tsx
+++ src/components/TextComponent.tsx
@@ -5,6 +5,7 @@ export const TextComponent = (props: FlexProps) => {
     const [displayedText, setDisplayedText] = useState("");
     const [loading, setLoading] = useState(true);
     const [isInView, setIsInView] = useState(false);
+    const [error, setError] = useState(false);
     const text = "A US F-22 fighter jet has shot down a cylindrical object in Canadian airspace on Saturday, the second such incident in two days. The US military also scrambled fighter jets to investigate a radar anomaly in Montana. Canadian Prime Minister Justin Trudeau announced the shootdown in the northern Yukon territory, adding that Canadian forces would recover and analyze the wreckage. The Canadian Defense Minister declined to speculate on the origin of the object but said it posed a risk to civilian air traffic and was shot down using an AIM 9X missile. The Pentagon said that the NORAD detected the object over Alaska on Friday and U.S. fighter jets monitored it as it crossed into Canadian airspace, where Canadian CF-18 and CP-140 aircraft joined the formation. The U.S. President Joe Biden authorized the U.S. military to work with Canada to take down the high-altitude craft."
     const textRef = useRef<HTMLDivElement>(null);
     const [isMobile, setMobile] = useState(false);
@@ -45,11 +46,17 @@ export const TextComponent = (props: FlexProps) => {
         let intervalId = null;
         if (loading && isInView) {
             intervalId = setInterval(() => {
-                setDisplayedText(text.slice(0, displayedText.length + 1));
-                textRef.current.scrollTop = textRef.current.scrollHeight;
                 if (displayedText.length === text.length) {
                     setLoading(false);
                     clearInterval(intervalId);
+                } else if (displayedText.length < text.length) {
+                    try {
+                        setDisplayedText(text.slice(0, displayedText.length + 1));
+                        textRef.current.scrollTop = textRef.current.scrollHeight;
+                    } catch (error) {
+                        setError(true);
+                        clearInterval(intervalId);
+                    }
                 }
             }, 66);
         }
@@ -64,7 +71,7 @@ export const TextComponent = (props: FlexProps) => {
                 <Box minWidth="50%" height={84} maxWidth={900} backgroundImage={isMobile ? "linear-gradient(to right, #76AB9B 0%, #76AB9B 10%, #fff 40%, #fff 100%)" : "linear-gradient(to bottom, #75AA9B 0%, #75AA9B 100%)"} padding={{ xl: "1rem", base: "0.75rem" }} marginTop="1.84rem" borderRadius="0.5rem" display="flex" alignItems="center">
                     <Heading paddingLeft={isMobile && "5rem"} width="calc(100vw - 3rem)" maxWidth={900} fontSize={{ xl: "xl", base: "md" }} color="#000" textAlign={isMobile ? "start" : "center"}>
                         <Tooltip label="World News">
-                            <p style={{  fontWeight: 800, textTransform: 'capitalize' }}>U.S. fighter jet shoots down unidentified cylindrical object over Canada</p>
+                            <p style={{ userSelect: 'none', fontWeight: 800, textTransform: 'capitalize' }}>U.S. fighter jet shoots down unidentified cylindrical object over Canada</p>
                         </Tooltip>
                     </Heading>
                 </Box>
